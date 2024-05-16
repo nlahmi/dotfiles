088832563b17bfa3357ebdeda5a922808782adce
nlahmi <mogobekl123@gmail.com>
2024-05-16 21:49:46 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Install chezmoi
-sh -c "$(curl -fsLS get.chezmoi.io)"
-
-# Apply
+# Install chezmoi and Pull configurations
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply nlahmi
 
 # TODO: Add nvim dependencies (or run its own setup script)
