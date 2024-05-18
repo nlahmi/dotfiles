5c875b4a046dd3f932154786a6ea7b372552f305
nlahmi <mogobekl123@gmail.com>
2024-05-18 15:13:27 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
+sudo apt update
+sudo apt install curl git zsh fzf wezterm ... -y
+
+# Install starship
+curl -sS https://starship.rs/install.sh | sh -s -- -f
+
+# Install fzf (in apt it's currently an outdated version)
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --key-bindings --completion --no-update-rc
+
+# Install Zoxide
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+
+# Set zsh as the defualt shell for the current user
+sudo chsh $USER -s /bin/zsh
+
 # Install chezmoi and Pull configurations
-sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply nlahmi
+sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin init --apply nlahmi
 
 # TODO: Add nvim dependencies (or run its own setup script)
