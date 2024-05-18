65c716a163374818eb2bfbb4eb86097148bdeeb4
nlahmi <mogobekl123@gmail.com>
2024-05-18 15:13:51 +0300

    Update setup.sh to add zsh and friends

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install curl git zsh fzf wezterm ... -y
+sudo apt install curl git zsh fzf wezterm -y
 
 # Install starship
 curl -sS https://starship.rs/install.sh | sh -s -- -f
