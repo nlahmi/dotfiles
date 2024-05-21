fc7c35956f56be464796310c2960669d0202beca
nlahmi <mogobekl123@gmail.com>
2024-05-22 00:54:20 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-# Install wezterm
+# Wezterm
 curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
 echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
 
-# Install Helm
+# Helm
 curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
-sudo apt-get install apt-transport-https --yes
+sudo apt install apt-transport-https --yes
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 
-# TODO: Install kubectl and k9s
+# Kubectl
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg # allow unprivileged APT programs to read this keyring
+
+# k9s
+curl -sS https://webi.sh/k9s | sh
 
 sudo apt update
-sudo apt install curl git zsh wezterm helm -y
+sudo apt install curl git zsh wezterm helm kubectl -y
 
 # Install starship
 curl -sS https://starship.rs/install.sh | sh -s -- -f
