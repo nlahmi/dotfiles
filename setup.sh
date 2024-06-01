09882f516e03a2abceffa46acfd0a835fc8ca8e9
nlahmi <mogobekl123@gmail.com>
2024-06-01 18:21:46 +0300

    Added lazygit install

--- setup.sh
+++ setup.sh
@@ -29,6 +29,13 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 # Install Zoxide
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
 
+# Install lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+rm lazygit.tar.gz
+
 # Set zsh as the defualt shell for the current user
 sudo chsh $USER -s /bin/zsh
 
