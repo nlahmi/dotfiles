dda1ad909724c004fc43d831de98a20f41de1b27
n.lahmi <mogobekl123@gmail.com>
2026-05-05 18:34:55 +0300

    added kanata to install

--- setup.ps1
+++ setup.ps1
@@ -16,6 +16,23 @@ powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | ie
 # Apply Chezmoi
 chezmoi init --apply nlahmi
 
+# Install Kanata
+$kanataZip = "$env:TEMP\kanata.zip"
+$kanataExtract = "$env:TEMP\kanata"
+Invoke-WebRequest -Uri "https://github.com/jtroo/kanata/releases/download/v1.11.0/windows-binaries-x64.zip" -OutFile $kanataZip
+Expand-Archive -Path $kanataZip -DestinationPath $kanataExtract -Force
+New-Item -ItemType Directory -Force -Path "C:\Program Files\Kanata"
+Copy-Item "$kanataExtract\kanata_windows_tty_winIOv2_x64.exe" "C:\Program Files\Kanata\kanata.exe"
+Copy-Item "$kanataExtract\kanata_windows_gui_winIOv2_x64.exe" "C:\Program Files\Kanata\kanata_gui.exe"
+Remove-Item $kanataZip, $kanataExtract -Recurse -Force
+
+# Create startup shortcut for Kanata
+$shell = New-Object -ComObject WScript.Shell
+$shortcut = $shell.CreateShortcut("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\kanata_gui.exe.lnk")
+$shortcut.TargetPath = "C:\Program Files\Kanata\kanata_gui.exe"
+$shortcut.Arguments = "-c `"$env:USERPROFILE\.config\kanata\kanata.kbd`""
+$shortcut.Save()
+
 # Install Neovim
 git clone https://github.com/nlahmi/nvim-config $env:LOCALAPPDATA\nvim
 cd $env:LOCALAPPDATA\nvim
